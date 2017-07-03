a1ae7eb75fdd270a2e1e23f6b9ae81ec109f5def
Alberto Asuero <alberto@geographica.gs>
2017-07-03 16:58:02 +0200

    CARTO basemap in globe

--- src/app/landing/globe/globe.component.ts
+++ src/app/landing/globe/globe.component.ts
@@ -8,11 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
-    debugger;
-    var viewer = new Cesium.Viewer('cesiumContainer');
+
+    var positron = new Cesium.UrlTemplateImageryProvider({
+        url : 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+    });
+
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        imageryProvider: positron,
+        baseLayerPicker: false,
+        fullscreenButton: false,
+        homeButton: false,
+        timeline: false,
+        navigationHelpButton: false,
+        animation: false,
+        scene3DOnly: true,
+        geocoder: false,
+        infoBox: false,
+        vrButton: false,
+        creditContainer: 'cesiumCredit'
+    });
+
+    // var dataSource = new Cesium.GeoJsonDataSource();
+    // viewer.dataSources.add(dataSource);
+    // var sql = new cartodb.SQL({user: 'cartotraining', format: 'geoJSON'});
+    // sql.execute("SELECT * FROM ne_10m_populated_places_simple WHERE adm0_a3 like 'USA' AND pop_max > 1000000")
+    //         .done(function (data) {
+    //             dataSource.load(data)
+    //                     .then(function () {
+    //                     })
+    //         })
+    //         .error(function (errors) {
+    //             // errors contains a list of errors
+    //             console.log("errors:" + errors);
+    //         });
   }
 
 }
