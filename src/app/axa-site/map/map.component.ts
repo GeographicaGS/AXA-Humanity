c3d1b948a891381aad7cec8ab534588c8b583907
Eduardo Amador Patón <eamadorpaton@gmail.com>
2017-07-03 14:39:53 +0200

    1 - map component base

--- src/app/axa-site/map/map.component.ts
+++ src/app/axa-site/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var cdb: any;
+declare var L: any;
+
 @Component({
   selector: 'app-axa-map',
   templateUrl: './map.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map: any = {};
+  options: any = {
+    zoomControl: false,
+    center: L.latLng(19.476950206488414, -1.58203125),
+    zoom: 2,
+    minZoom: 1,
+    maxZoom: 19,
+    layers: [],
+    vizUrl: '',
+    markerRadius: 7
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.map = L.map('map', {
+      zoomControl: this.options.zoomControl,
+      center: this.options.center,
+      zoom: this.options.zoom,
+      minZoom: this.options.minZoom,
+      maxZoom: this.options.maxZoom,
+      layers: [
+         L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{style}/{z}/{x}/{y}.png',
+         { style: 'light_all', zIndex: 0 } )
+      ]
+    });
   }
 
 }
