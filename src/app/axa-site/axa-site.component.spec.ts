b9f2591d117095d0e8a584447f295370ca228fec
Eduardo Amador Patón <eamadorpaton@gmail.com>
2017-07-03 12:10:51 +0200

    4 - tests and lint fixes

---
+++ src/app/axa-site/axa-site.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AxaSiteComponent } from './axa-site.component';
+import { AxaSiteModule } from './axa-site.module';
+import { CommonModule } from '../common/common.module';
+
+describe('AxaSiteComponent', () => {
+  let component: AxaSiteComponent;
+  let fixture: ComponentFixture<AxaSiteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [CommonModule, AxaSiteModule],
+      providers:  [
+        {provide: APP_BASE_HREF, useValue : '/' }
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AxaSiteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  // We need to import CARTODB.js
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/axa-site/axa-site.component.ts
+++ src/app/axa-site/axa-site.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'axa-site',
+  selector: 'app-axa-site',
   templateUrl: './axa-site.component.html',
   styleUrls: ['./axa-site.component.scss']
 })
