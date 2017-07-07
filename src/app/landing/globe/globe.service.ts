020ba1e39d14cdc922d633d507c9eec791967e12
Paula Juliá <paula.julia@geographica.gs>
2017-07-07 13:09:40 +0200

    new_globe_data

--- src/app/landing/globe/globe.service.ts
+++ src/app/landing/globe/globe.service.ts
@@ -19,10 +19,28 @@ const DATA = [{
     zone: 'South America'
   },
   {
+    type: 'dark',
+    value: 69,
+    units: '% of entrepreneurs women have',
+    title: '0 €',
+    subtitle: 'on protection or savings oriented life insurance',
+    position : [10, 48],
+    zone: 'Europe'
+  },
+  {
+    type: 'dark',
+    value: 70,
+    units: 'cm of rise in the sea level in this century',
+    title: '100 million people',
+    subtitle: 'will be displaced by the elevation of waters',
+    position : [-41, 66],
+    zone: 'Poles'
+  }
+  {
     type: 'light',
     value: 1,
     units: 'World Ranking of Insurer Innovation',
-    title: '30',
+    title: 'Nº 30',
     subtitle: 'from all sectors',
     position : [47, 2],
     zone: 'AXA Worldwide'
@@ -35,6 +53,24 @@ const DATA = [{
     subtitle: 'supported by AXA Research Fund',
     position : [13, 12],
     zone: 'AXA Worldwide'
+  },
+  {
+    type: 'light',
+    value: 20,279,
+    units: 'new hires',
+    title: 'Per year',
+    subtitle: 'worldwide',
+    position : [13, 12],
+    zone: 'AXA Worldwide'
+  },
+  {
+    type: 'light',
+    value: 4,000,
+    units: 'new policies in its first month',
+    title: 'Smart Women Plan',
+    subtitle: 'Product of the Year, 2013',
+    position : [78, 20],
+    zone: 'India'
   }];
 
 export class GlobeService {
