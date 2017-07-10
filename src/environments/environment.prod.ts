802f00aba43a313c333e7d0a8a866949de3c1f39
Eduardo Amador Patón <eamadorpaton@gmail.com>
2017-07-10 13:12:18 +0200

    Ng lint warning fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,71 +3,72 @@ export const environment = {
   useHash: true,
   goals: [
     {
-      "id": 1,
-      "text": "No poverty"
+      'id': 1,
+      'text': 'No poverty'
     },
     {
-      "id": 2,
-      "text": "Zero hunger"
+      'id': 2,
+      'text': 'Zero hunger'
     },
     {
-      "id": 3,
-      "text": "Good health and well-being"
+      'id': 3,
+      'text': 'Good health and well-being'
     },
     {
-      "id": 4,
-      "text": "Quality education"
+      'id': 4,
+      'text': 'Quality education'
     },
     {
-      "id": 5,
-      "text": "Gender equality"
+      'id': 5,
+      'text': 'Gender equality'
     },
     {
-      "id": 6,
-      "text": "Clean water and sanitation"
+      'id': 6,
+      'text': 'Clean water and sanitation'
     },
     {
-      "id": 7,
-      "text": "Affordable and clean energy"
+      'id': 7,
+      'text': 'Affordable and clean energy'
     },
     {
-      "id": 8,
-      "text": "Decent work and economic growth"
+      'id': 8,
+      'text': 'Decent work and economic growth'
     },
     {
-      "id": 9,
-      "text": "Industry, innovation and infrastructure"
+      'id': 9,
+      'text': 'Industry, innovation and infrastructure'
     },
     {
-      "id": 10,
-      "text": "Reduced inequalities"
+      'id': 10,
+      'text': 'Reduced inequalities'
     },
     {
-      "id": 11,
-      "text": "Sustainable cities and communities"
-    },{
-      "id": 12,
-      "text": "Responsible consumption and production"
+      'id': 11,
+      'text': 'Sustainable cities and communities'
     },
     {
-      "id": 13,
-      "text": "Climate action"
+      'id': 12,
+      'text': 'Responsible consumption and production'
     },
     {
-      "id": 14,
-      "text": "Life below water"
+      'id': 13,
+      'text': 'Climate action'
     },
     {
-      "id": 15,
-      "text": "Life on land"
+      'id': 14,
+      'text': 'Life below water'
     },
     {
-      "id": 16,
-      "text": "Peace, justice and strong institutions"
+      'id': 15,
+      'text': 'Life on land'
     },
     {
-      "id": 17,
-      "text": "Partnerships for the goals"
+      'id': 16,
+      'text': 'Peace, justice and strong institutions'
+    },
+    {
+      'id': 17,
+      'text': 'Partnerships for the goals'
     }
   ]
 };
