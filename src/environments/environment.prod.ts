65b6d19fff9c4fcc33e7f221aaf35a175cff20a6
Eduardo Amador Patón <eamadorpaton@gmail.com>
2017-07-10 13:08:39 +0200

    Goals images changed - Tooltip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,73 @@
 export const environment = {
   production: true,
-  useHash: true
+  useHash: true,
+  goals: [
+    {
+      "id": 1,
+      "text": "No poverty"
+    },
+    {
+      "id": 2,
+      "text": "Zero hunger"
+    },
+    {
+      "id": 3,
+      "text": "Good health and well-being"
+    },
+    {
+      "id": 4,
+      "text": "Quality education"
+    },
+    {
+      "id": 5,
+      "text": "Gender equality"
+    },
+    {
+      "id": 6,
+      "text": "Clean water and sanitation"
+    },
+    {
+      "id": 7,
+      "text": "Affordable and clean energy"
+    },
+    {
+      "id": 8,
+      "text": "Decent work and economic growth"
+    },
+    {
+      "id": 9,
+      "text": "Industry, innovation and infrastructure"
+    },
+    {
+      "id": 10,
+      "text": "Reduced inequalities"
+    },
+    {
+      "id": 11,
+      "text": "Sustainable cities and communities"
+    },{
+      "id": 12,
+      "text": "Responsible consumption and production"
+    },
+    {
+      "id": 13,
+      "text": "Climate action"
+    },
+    {
+      "id": 14,
+      "text": "Life below water"
+    },
+    {
+      "id": 15,
+      "text": "Life on land"
+    },
+    {
+      "id": 16,
+      "text": "Peace, justice and strong institutions"
+    },
+    {
+      "id": 17,
+      "text": "Partnerships for the goals"
+    }
+  ]
 };
